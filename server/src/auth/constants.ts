c57cd90337929c28a19a454a78fce5cad713f63d
Diogo Madeira <diogomadeira10@gmail.com>
2023-11-08 22:26:28 +0000

    feat: authorization

---
+++ server/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'dPemxT9GuNUCDPV5pctYCWLQFzKd05w3YpFEnWcH', // ! hard coded only for the recruiting challenge
+};
--- server/src/customers/customers.controller.ts
+++ server/src/customers/customers.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CustomersService } from './customers.service';
 import { Customer } from './schemas/customer.schema';
 
@@ -19,16 +20,19 @@ export class CustomersController {
       return this.customersService.getCustomers();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async createCustomer(@Body() createCustomerDto: CreateCustomerDto): Promise<Customer> {
       return this.customersService.createCustomer(createCustomerDto.name, createCustomerDto.surname, createCustomerDto.birthday)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':customerId')
   async updateCustomer(@Param('customerId') customerId: string, @Body() updateCustomerDto: UpdateCustomerDto): Promise<Customer> {
       return this.customersService.updateCustomer(customerId, updateCustomerDto);
   }
   
+  @UseGuards(JwtAuthGuard)
   @Delete(':customerId')
   async deleteCustomer(@Param('customerId') customerId: string): Promise<Customer> {
       return this.customersService.deleteCustomer(customerId);
